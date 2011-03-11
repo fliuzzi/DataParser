ff5f331b29a77a094418e3a0f07ce6a5f5259a01
Michael Mastroianni <mmastroianni@paypal.com>
2011-03-11 23:00:15 +0000

    fixed moronism

--- src/com/where/atlas/feed/citysearch/FixCatsInLis.java
+++ src/com/where/atlas/feed/citysearch/FixCatsInLis.java
@@ -44,6 +44,7 @@ public class FixCatsInLis
                 catNames.append(cat.getName());
                 if(cnt > 0) {catIds.append(",");}
                 catIds.append(cat.getId());
+		cnt++
             }
             
             doc.add(new Field("catnames", catNames.toString(), Field.Store.YES, Index.ANALYZED_NO_NORMS));
