5deeabedaefb3017636b8769801a77f375f3afc5
qimeng <qimeng@where.com>
2011-09-14 12:07:28 -0400

    update GetRatings and Yelp80legsParser

--- src/com/where/atlas/feed/yelp/GetRatings.java
+++ src/com/where/atlas/feed/yelp/GetRatings.java
@@ -17,73 +17,76 @@ import org.json.JSONObject;
 public class GetRatings {
 	static FileReader ids_fileReader = null;
 	static BufferedReader ids_buff = null;
-	static String ids_input_dir = "/home/qimeng/yelp-example.txt";
+	static String ids_input_dir = "/home/qimeng/yelp.output/";
 
 	static FileReader json_fileReader = null;
 	static BufferedReader json_buff = null;
-	static String json_input_dir = "/home/qimeng/inputjson/";
+	static String json_input_dir = "/home/qimeng/GetRating/inputjson/";
 	static HashMap<Long, Long> hashMap = new HashMap<Long, Long>();
 
 	static FileWriter result_fileWriter = null;
-	static String result_dir = "/home/qimeng/outPutForTest";
+	static String result_dir = "/home/qimeng/GetRating/outPutForTest";
 	static BufferedWriter result_buff = null;
 
 	static AtomicLong atomicN = new AtomicLong();
 	static HashMap<String, Long> id_map = new HashMap<String, Long>();
-	static String id_hash_dir = "/home/qimeng/id_hash";
+	static String id_hash_dir = "/home/qimeng/GetRating/id_hash";
 	static FileWriter id_hash_file = null;
 	static BufferedWriter id_hash_buff = null;
 	public static final void main(String[] args){
 		try {
+			File ids_input_directory = new File(ids_input_dir);
+			File[] ids_input_files = ids_input_directory.listFiles();
+			for(int x=0; x<ids_input_files.length; x++){
+				ids_fileReader = new FileReader(ids_input_files[x]);
+				ids_buff = new BufferedReader(ids_fileReader);
+				String input_line = null;
 
-			ids_fileReader = new FileReader(ids_input_dir);
-			ids_buff = new BufferedReader(ids_fileReader);
-			String input_line = null;
+				//			test version
+				//			while((input_line = ids_buff.readLine()) != null){
+				//
+				//				String[] inputs = input_line.split(",");
+				//
+				//				Long yelp_id = Long.parseLong(inputs[0]);
+				//				Long where_id = Long.parseLong(inputs[1]);
+				//				hashMap.put(yelp_id, where_id);
+				//			}
 
-			//			test version
-			//			while((input_line = ids_buff.readLine()) != null){
-			//
-			//				String[] inputs = input_line.split(",");
-			//
-			//				Long yelp_id = Long.parseLong(inputs[0]);
-			//				Long where_id = Long.parseLong(inputs[1]);
-			//				hashMap.put(yelp_id, where_id);
-			//			}
+				//			generate id HashMap from id pairs, 
+				//			samples:
+				//			y:10100 1389	4 1 0 0 (0.9) <y:10100 Bank Of America> <1389 Bank Of America>
+				//			y:10100 198	4 1 0 0 (0.9) <y:10100 Bank Of America> <198 Bank Of America>
+				//			y:10100 199	4 1 0 0 (0.9) <y:10100 Bank Of America> <199 Bank Of America>
+				//			10100 y:2830	4 1 0 0 (0.9) <10100 Bank Of America> <y:2830 Bank Of America>
+				//			10100 y:3102	4 1 0 0 (0.9) <10100 Bank Of America> <y:3102 Bank Of America>
+				//			10100 y:5297	4 1 0 0 (0.9) <10100 Bank Of America> <y:5297 Bank Of America>
+				//			y:10100 y:5500	4 1 0 0 (0.9) <y:10100 Bank Of America> <y:5500 Bank Of America>
+				//			y:10125 y:10203	4 1 0 0 (0.9) <y:10125 Public Storage> <y:10203 Public Storage>
+				//			y:10125 y:1374	4 1 0 0 (0.9) <y:10125 Public Storage> <y:1374 Public Storage>
 
-			//			generate id HashMap from id pairs, 
-			//			samples:
-			//			y:10100 1389	4 1 0 0 (0.9) <y:10100 Bank Of America> <1389 Bank Of America>
-			//			y:10100 198	4 1 0 0 (0.9) <y:10100 Bank Of America> <198 Bank Of America>
-			//			y:10100 199	4 1 0 0 (0.9) <y:10100 Bank Of America> <199 Bank Of America>
-			//			10100 y:2830	4 1 0 0 (0.9) <10100 Bank Of America> <y:2830 Bank Of America>
-			//			10100 y:3102	4 1 0 0 (0.9) <10100 Bank Of America> <y:3102 Bank Of America>
-			//			10100 y:5297	4 1 0 0 (0.9) <10100 Bank Of America> <y:5297 Bank Of America>
-			//			y:10100 y:5500	4 1 0 0 (0.9) <y:10100 Bank Of America> <y:5500 Bank Of America>
-			//			y:10125 y:10203	4 1 0 0 (0.9) <y:10125 Public Storage> <y:10203 Public Storage>
-			//			y:10125 y:1374	4 1 0 0 (0.9) <y:10125 Public Storage> <y:1374 Public Storage>
+				while((input_line = ids_buff.readLine()) != null){
+					String[] inputs = input_line.split("\\s".intern());
 
-			while((input_line = ids_buff.readLine()) != null){
-				String[] inputs = input_line.split("\\s".intern());
-
-				if(inputs[0].startsWith("y".intern())){
-					if(!inputs[1].startsWith("y".intern())){
-						long yelp_id = Long.parseLong(inputs[0].substring(2));
-						long where_id = Long.parseLong(inputs[1]); 
-						hashMap.put(yelp_id, where_id);
+					if(inputs[0].startsWith("y".intern())){
+						if(!inputs[1].startsWith("y".intern())){
+							long yelp_id = Long.parseLong(inputs[0].substring(2));
+							long where_id = Long.parseLong(inputs[1]); 
+							hashMap.put(yelp_id, where_id);
+						}
+						//else, both are yelp id, ignore wrong input
 					}
-					//else, both are yelp id, ignore wrong input
-				}
 
-				else {
-					if (inputs[1].startsWith("y".intern())){
-						long yelp_id = Long.parseLong(inputs[0]);
-						long where_id = Long.parseLong(inputs[1].substring(2)); 
-						hashMap.put(yelp_id, where_id);
+					else {
+						if (inputs[1].startsWith("y".intern())){
+							long yelp_id = Long.parseLong(inputs[0]);
+							long where_id = Long.parseLong(inputs[1].substring(2)); 
+							hashMap.put(yelp_id, where_id);
+						}
+						//else, both are where id, ignore
 					}
-					//else, both are where id, ignore
 				}
+				System.out.println((x+1)+" id file processed");
 			}
-
 			System.out.println("HashMap done!!!!");
 
 			result_fileWriter = new FileWriter(result_dir);
@@ -93,12 +96,12 @@ public class GetRatings {
 			id_hash_buff = new BufferedWriter(id_hash_file);
 			System.out.println("initialized buffers");
 
-			File input_directory = new File(json_input_dir);
-			final File[] input_files = input_directory.listFiles();
+			File json_input_directory = new File(json_input_dir);
+			final File[] json_input_files = json_input_directory.listFiles();
 
 			int counter = 0;
-			for(int i=0; i<input_files.length;  i++){
-				json_fileReader = new FileReader(input_files[i]);
+			for(int i=0; i<json_input_files.length;  i++){
+				json_fileReader = new FileReader(json_input_files[i]);
 				json_buff = new BufferedReader(json_fileReader);
 
 				String json_line = null;
@@ -123,14 +126,11 @@ public class GetRatings {
 									else {
 										userId = id_map.get(userHash);
 									}
-
-//																		System.out.println(user.optString("user_id")+","+place_id+","+user.optString("rating"));
 									result_buff.write(userId +","+place_id+","+user.optString("rating"));
 									result_buff.newLine();
 									result_buff.flush();
 								}
-							}
-
+							} 
 						}
 					} catch (JSONException e) {
 						e.printStackTrace();
@@ -142,6 +142,7 @@ public class GetRatings {
 					System.out.println(counter + " files processed");
 				}
 			}
+			System.out.println("All set!!!!!!!!");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
--- src/com/where/atlas/feed/yelp/Yelp80legsParser.java
+++ src/com/where/atlas/feed/yelp/Yelp80legsParser.java
@@ -8,7 +8,10 @@ import java.io.InputStream;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.apache.commons.lang.StringEscapeUtils;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -24,12 +27,12 @@ public class Yelp80legsParser implements FeedParser{
 
 	private static Boolean WANT_REVIEW_TEXT;
 	private static BufferedWriter bufferedWriter;
-	
+
 	public Yelp80legsParser(String writePath) throws IOException
 	{
 		bufferedWriter = new BufferedWriter(new FileWriter(writePath));
 	}
-	
+
 	public void setFlag(String flg){
 		if(flg.equals("true"))
 			WANT_REVIEW_TEXT = true;
@@ -37,161 +40,173 @@ public class Yelp80legsParser implements FeedParser{
 			WANT_REVIEW_TEXT = false;
 		}
 		else{
-			
+
 		}
 	}
-	
+
 	public static BufferedWriter getWriter()
 	{
 		return bufferedWriter;
 	}
-	
+
 	@Override
 	public void parse(PlaceCollector collector, InputStream ins)
-			throws Exception {
-            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-            Document doc = docBuilder.parse(ins);
-            
-            doc.getDocumentElement().normalize();
-            
-            
-        	NodeList listOfListings = doc.getElementsByTagName("listing");
-        	parseListings(collector, listOfListings);
-            
+	throws Exception {
+		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse(ins);
+
+		doc.getDocumentElement().normalize();
+
+
+		NodeList listOfListings = doc.getElementsByTagName("listing");
+		parseListings(collector, listOfListings);
+
 	}
-	
-	
-    private void parseListings(PlaceCollector collector,NodeList listings)
-    {
-    	Node listingNode = null;
-        YelpPlace poi = null;
-        Address location = null;
-        //// <LISTING>
-        for(int i = 0; i < listings.getLength();i++)
-        {   
-        	
-            listingNode = listings.item(i);
-            if(listingNode.getNodeType() == Node.ELEMENT_NODE){
-                Element listingElement = (Element)listingNode;
-                
-                
-                poi = new YelpPlace();
-                location = new Address();
-                
-                
-                poi.setName(listingElement.getAttribute("name"));//NAME
-                poi.setPhone(YPRawDataParser.stripPhone(listingElement.getAttribute("phone")));//PHONE
-                poi.setBiz_url(listingElement.getAttribute("biz_url"));
-                poi.setAvg_rating(listingElement.getAttribute("overall_rating"));
-                poi.setWheelchair(listingElement.getAttribute("wheelchair"));
-                poi.setParking(listingElement.getAttribute("parking"));
-                poi.setPrice(listingElement.getAttribute("price"));
-                poi.setType(listingElement.getAttribute("type"));
-                
-                
-                
-                location.setZip(listingElement.getAttribute("postal_code"));//addy.ZIP
-                location.setCity(listingElement.getAttribute("locality"));//addy.CITY
-                location.setState(listingElement.getAttribute("region"));
-                location.setAddress1(listingElement.getAttribute("address"));
-                               
-                if(listingElement.getAttribute("lat").length() > 1 && listingElement.getAttribute("lon").length() > 1){
-                    location.setLat(Double.parseDouble(listingElement.getAttribute("lat")));	//LATITUDE
-                    location.setLng(Double.parseDouble(listingElement.getAttribute("lon")));	//LONGITUDE
-                }
-                
-                poi.setAddress(location);
-                
-                //sub-nodes
-                NodeList categories = listingElement.getElementsByTagName("category");
-                fillCategories(categories,poi);
-                NodeList reviews = listingElement.getElementsByTagName("reviews");
-                fillReviews(reviews,poi);
-                
-                NodeList URLlist = listingElement.getElementsByTagName("URL");
-                Element URL = (Element) URLlist.item(0);
-                
-                try{
-                	poi.setYelp_url(URL.getTextContent());
-                }
-                catch(NullPointerException np)
-                {
-                	
-                }
-                	
-                if(poi.getName().length() <= 0)
-            		collector.collectBadInput(poi, new Exception("Nullname"));
-                else
-                	collector.collect(poi);
-            }
-        }
-    	
-    }
-    
-    private static String removeNonUtf8CompliantCharacters( final String inString ) {
-    	if (null == inString ) return null;
-    		byte[] byteArr = inString.getBytes();
-    	for ( int i=0; i < byteArr.length; i++ ) {
-    		byte ch= byteArr[i];
-    		// remove any characters outside the valid UTF-8 range as well as all control characters
-    		// except tabs and new lines
-    		if ( !( (ch > 31 && ch < 253 ) || ch == '\t' || ch == '\n' || ch == '\r') ) {
-    			byteArr[i]=' ';
-    		}
-    	}
-    	return new String( byteArr );
-    }
-    
-    private void fillReviews(NodeList reviewList,YelpPlace poi)
-    {
-    	for(int i = 0;i < reviewList.getLength();i++)
-        {
-    		Node reviewNode = reviewList.item(i);
-        	if(reviewNode.getNodeType() == Node.ELEMENT_NODE){
-                Element review = (Element)reviewNode;
-                JSONObject json = new JSONObject();
-                
-                try{
-	                json.put("user", review.getAttribute("user_name"));
-	                json.put("user_id",review.getAttribute("user_id"));
-	                json.put("rating", review.getAttribute("rating"));
-	                json.put("date", review.getAttribute("date"));
-	                if(WANT_REVIEW_TEXT){
-	                	json.put("text", removeNonUtf8CompliantCharacters(review.getAttribute("text")));
-	                }
-	                if(review.getAttribute("useful").length() > 0)
-	                	json.put("useful", review.getAttribute("useful"));
-	                if(review.getAttribute("funny").length() > 0)
-	                	json.put("funny", review.getAttribute("funny"));
-	                if(review.getAttribute("cool").length() > 0)
-	                	json.put("cool",review.getAttribute("cool"));
-	                
-	                poi.addReview(json);
-                }
-                catch(Exception e)
-                {
-                	System.out.println("Error constructing review json: "+e.getMessage());
-                }
-                
-                
-                
-        	}
-        }
-    }
-    
-    private void fillCategories(NodeList categoryList,YelpPlace poi)
-    {
-    	for(int i = 0;i < categoryList.getLength();i++)
-        {
-    		Node categoryNode = categoryList.item(i);
-        	if(categoryNode.getNodeType() == Node.ELEMENT_NODE){
-                Element category = (Element)categoryNode;
-                
-                if(category.getAttribute("name") != null)
-                	poi.addCategory(category.getAttribute("name"));
-        	}
-        }
-    }
+
+
+	private void parseListings(PlaceCollector collector,NodeList listings)
+	{
+		Node listingNode = null;
+		YelpPlace poi = null;
+		Address location = null;
+		//// <LISTING>
+		for(int i = 0; i < listings.getLength();i++)
+		{   
+
+			listingNode = listings.item(i);
+			if(listingNode.getNodeType() == Node.ELEMENT_NODE){
+				Element listingElement = (Element)listingNode;
+
+
+				poi = new YelpPlace();
+				location = new Address();
+
+
+				poi.setName(StringEscapeUtils.unescapeHtml(listingElement.getAttribute("name")));//NAME
+				poi.setPhone(YPRawDataParser.stripPhone(listingElement.getAttribute("phone")));//PHONE
+				poi.setBiz_url(StringEscapeUtils.unescapeHtml(listingElement.getAttribute("biz_url")));
+				poi.setAvg_rating(StringEscapeUtils.unescapeHtml(listingElement.getAttribute("overall_rating")));
+				poi.setWheelchair(StringEscapeUtils.unescapeHtml(listingElement.getAttribute("wheelchair")));
+				poi.setParking(StringEscapeUtils.unescapeHtml(listingElement.getAttribute("parking")));
+				poi.setPrice(StringEscapeUtils.unescapeHtml(listingElement.getAttribute("price")));
+				poi.setType(StringEscapeUtils.unescapeHtml(listingElement.getAttribute("type")));
+
+
+
+				location.setZip(StringEscapeUtils.unescapeHtml(listingElement.getAttribute("postal_code")));//addy.ZIP
+				location.setCity(StringEscapeUtils.unescapeHtml(listingElement.getAttribute("locality")));//addy.CITY
+				location.setState(StringEscapeUtils.unescapeHtml(listingElement.getAttribute("region")));
+				location.setAddress1(StringEscapeUtils.unescapeHtml(listingElement.getAttribute("address")));
+
+				if(listingElement.getAttribute("lat").length() > 1 && listingElement.getAttribute("lon").length() > 1){
+					location.setLat(Double.parseDouble(listingElement.getAttribute("lat")));	//LATITUDE
+					location.setLng(Double.parseDouble(listingElement.getAttribute("lon")));	//LONGITUDE
+				}
+
+				poi.setAddress(location);
+
+				//sub-nodes
+				NodeList categories = listingElement.getElementsByTagName("category");
+				try {
+					fillCategories(categories,poi);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				NodeList reviews = listingElement.getElementsByTagName("reviews");
+				fillReviews(reviews,poi);
+
+				NodeList URLlist = listingElement.getElementsByTagName("URL");
+				Element URL = (Element) URLlist.item(0);
+
+				try{
+					poi.setYelp_url(URL.getTextContent());
+				}
+				catch(NullPointerException np)
+				{
+
+				}
+
+				if(poi.getName().length() <= 0)
+					collector.collectBadInput(poi, new Exception("Nullname"));
+				else
+					collector.collect(poi);
+			}
+		}
+
+	}
+
+	private static String removeNonUtf8CompliantCharacters( final String inString ) {
+		if (null == inString ) return null;
+		byte[] byteArr = inString.getBytes();
+		for ( int i=0; i < byteArr.length; i++ ) {
+			byte ch= byteArr[i];
+			// remove any characters outside the valid UTF-8 range as well as all control characters
+			// except tabs and new lines
+			if ( !( (ch > 31 && ch < 253 ) || ch == '\t' || ch == '\n' || ch == '\r') ) {
+				byteArr[i]=' ';
+			}
+		}
+		return new String( byteArr );
+	}
+
+	private void fillReviews(NodeList reviewList,YelpPlace poi)
+	{
+		for(int i = 0;i < reviewList.getLength();i++)
+		{
+			Node reviewNode = reviewList.item(i);
+			if(reviewNode.getNodeType() == Node.ELEMENT_NODE){
+				Element review = (Element)reviewNode;
+				JSONObject json = new JSONObject();
+
+				try{
+					json.put("user", StringEscapeUtils.unescapeHtml(review.getAttribute("user_name")));
+					json.put("user_id",StringEscapeUtils.unescapeHtml(review.getAttribute("user_id")));
+					json.put("rating", StringEscapeUtils.unescapeHtml(review.getAttribute("rating")));
+					json.put("date", StringEscapeUtils.unescapeHtml(review.getAttribute("date")));
+					if(WANT_REVIEW_TEXT){
+						json.put("text", removeNonUtf8CompliantCharacters(review.getAttribute("text")));
+					}
+					if(review.getAttribute("useful").length() > 0)
+						json.put("useful", review.getAttribute("useful"));
+					if(review.getAttribute("funny").length() > 0)
+						json.put("funny", review.getAttribute("funny"));
+					if(review.getAttribute("cool").length() > 0)
+						json.put("cool",review.getAttribute("cool"));
+
+					poi.addReview(json);
+				}
+				catch(Exception e)
+				{
+					System.out.println("Error constructing review json: "+e.getMessage());
+				}
+
+
+
+			}
+		}
+	}
+
+	private void fillCategories(NodeList categoryList,YelpPlace poi)
+	{
+		for(int i = 0;i < categoryList.getLength();i++)
+		{
+			Node categoryNode = categoryList.item(i);
+			if(categoryNode.getNodeType() == Node.ELEMENT_NODE){
+				Element category = (Element)categoryNode;
+
+				if(category.getAttribute("name") != null)
+					try {
+						poi.addCategory(jsonStringDecode(StringEscapeUtils.unescapeHtml(category.getAttribute("name"))));
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+			}
+		}
+	}
+
+	private String jsonStringDecode(String string) throws JSONException { 
+		return ((String) new JSONTokener('"' + string + '"').nextValue()); 
+	}
 
 }
